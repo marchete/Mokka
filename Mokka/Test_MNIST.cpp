0195170f1e598c15cc548c925d5c845a36ba076c
marchete <marchete@users.noreply.github.com>
2021-06-29 09:37:04 +0200

    Dense layer optimizations
    Cleaned up code:
    -Removed Stopwatch and Random classes from NN_Mokka header. It shouldn't be there as that has nothing to do with the inference engine.
    -Removed CNN partial code, it's still incomplete so it stays in the previous version.
    -Alternative version of Dense Layer.
    
    Now Dense predict() takes into account the input value, the calculateOutput() for Dense code is simpler to read:
    1. output = bias
    2. For each input I:
           if val[I] == 0.0f continue
           if val[I] == 1.0f output += weights[I]
           else output +=  weights[I] * val[I]
    
    The 0.0f conditional is noticeable with one-hot and ReLU activations, it gives a big improvement. MNIST went from 12us/sample to 5us/sample

--- Mokka/Test_MNIST.cpp
+++ Mokka/Test_MNIST.cpp
@@ -41,6 +41,17 @@ Model CreateModel29(shared_ptr<Input>& input, shared_ptr<Layer>& policy, shared_
 	Model model({ input }, { policy });//("simple_test28Gray.ahsf", "weights", "mean");
 	return model;
 }
+#define Now() chrono::high_resolution_clock::now()
+struct Stopwatch {
+	chrono::high_resolution_clock::time_point c_time, c_timeout;
+	void Start(int us) { c_time = Now(); c_timeout = c_time + chrono::microseconds(us); }
+	void setTimeout(int us) { c_timeout = c_time + chrono::microseconds(us); }
+	inline bool Timeout() {
+		return Now() > c_timeout;
+	}
+	long long EllapsedMicroseconds() { return chrono::duration_cast<chrono::microseconds>(Now() - c_time).count(); }
+	long long EllapsedMilliseconds() { return chrono::duration_cast<chrono::milliseconds>(Now() - c_time).count(); }
+} stopwatch;
 
 /* Unfinished
 Model CreateCNN(shared_ptr<Input>& input, shared_ptr<Layer>& policy, shared_ptr<Layer>& value) {
